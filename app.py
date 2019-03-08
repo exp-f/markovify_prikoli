a2c041a7a4de324d978ea52e640f7f3f15e60ab2
artem@dobroagency.ru <cs1035idIII>
2019-03-08 16:09:13 +0400

    Праздничное обновление, я прав, парни?

--- app.py
+++ app.py
@@ -7,18 +7,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-msg = 'Реально прикол)'
+msg = ''
 
-c_text = open('datasets/chuvashia_facts.txt', 'r', encoding='UTF-8').read()
+ch_text = open('datasets/chuvashia_facts.txt', 'r', encoding='UTF-8').read()
 p_text = open('datasets/poslovicy.txt', 'r', encoding='UTF-8').read()
 t_text = open('datasets/tweets.txt', 'r', encoding='UTF-8').read()
 
 # слишком много пословиц бля, заебали
-p_text = '\n'.join(random.choices(p_text.split('\n'), k=500))
-c_model = markovify.NewlineText(c_text)
+# p_text = '\n'.join(random.choices(p_text.split('\n'), k=500))
+ch_model = markovify.NewlineText(ch_text)
 p_model = markovify.NewlineText(p_text)
-t_model = markovify.NewlineText(c_text)
-
+t_model = markovify.NewlineText(t_text)
+congrats_text = open('datasets/congrats.txt', 'r', encoding='UTF-8').read()
+c_model = markovify.NewlineText(congrats_text)
 port = sys.argv[1]
 
 
@@ -30,14 +31,18 @@ def combine_models(*args, weights=(1, 1, 1)):
 
 def thread_wrap():
     while True:
-        weights = [random.uniform(1.0, 2.0) for _ in range(3)]
-        global c_model, p_model, t_model
-        combine_models(c_model, p_model, t_model, weights=weights)
+        weights = [random.uniform(1.0, 2.0) for _ in range(4)]
+        # global c_model, p_model, t_model
+        # combine_models(c_model, p_model, t_model, weights=weights)
+        global msg, t_model, c_model, p_model, ch_model
+        combo_model = markovify.combine([t_model, c_model, p_model, ch_model], weights=weights)
+        msg = combo_model.make_short_sentence(280, tries=1000)
         sleep(3)
 
 
 combine_thread = Thread(target=thread_wrap)
 combine_thread.start()
+
 
 @app.route('/')
 def main():
