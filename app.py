277644e1c8447f1bc7662e9df2ba8d503668fecb
artem@dobroagency.ru <cs1035idIII>
2019-03-10 12:00:46 +0400

    Откатил праздничное обновление, парни

--- app.py
+++ app.py
@@ -13,31 +13,20 @@ ch_text = open('datasets/chuvashia_facts.txt', 'r', encoding='UTF-8').read()
 p_text = open('datasets/poslovicy.txt', 'r', encoding='UTF-8').read()
 t_text = open('datasets/tweets.txt', 'r', encoding='UTF-8').read()
 
-# слишком много пословиц бля, заебали
-# p_text = '\n'.join(random.choices(p_text.split('\n'), k=500))
 ch_model = markovify.NewlineText(ch_text)
 p_model = markovify.NewlineText(p_text)
 t_model = markovify.NewlineText(t_text)
 congrats_text = open('datasets/congrats.txt', 'r', encoding='UTF-8').read()
-c_model = markovify.NewlineText(congrats_text)
 port = sys.argv[1]
-
-
-def combine_models(*args, weights=(1, 1, 1)):
-    global msg
-    combo_model = markovify.combine(args, weights)
-    msg = combo_model.make_sentence(tries=1000)
 
 
 def thread_wrap():
     while True:
-        weights = [random.uniform(1.0, 2.0) for _ in range(4)]
-        # global c_model, p_model, t_model
-        # combine_models(c_model, p_model, t_model, weights=weights)
-        global msg, t_model, c_model, p_model, ch_model
-        combo_model = markovify.combine([t_model, c_model, p_model, ch_model], weights=weights)
+        weights = [random.uniform(1.0, 2.0) for _ in range(3)]
+        global msg, t_model, p_model, ch_model
+        combo_model = markovify.combine([t_model, p_model, ch_model], weights=weights)
         msg = combo_model.make_short_sentence(280, tries=1000)
-        sleep(3)
+        sleep(1)
 
 
 combine_thread = Thread(target=thread_wrap)
